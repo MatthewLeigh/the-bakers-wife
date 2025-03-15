999f70e874e84c59246810c068d5ba63ff3109bf
MatthewLeigh <matthewcross97@gmail.com>
2025-03-16 10:15:13 +1100

    Draft KeyInfo component

--- frontend/src/components/Home/KeyInfo/KeyInfo.tsx
+++ frontend/src/components/Home/KeyInfo/KeyInfo.tsx
@@ -7,8 +7,34 @@ import styles from "./KeyInfo.module.css"
 const KeyInfo = () => {
     return (
         <div className={styles.componentWrapper}>
+            <div className={styles.innerWrapper}>
 
+                {/* Location */}
+                <div className={styles.textBlockWrapper}>
+                    <p><strong>Close to Home</strong></p>
+                    <div className={styles.lowerText}>
+                        <p>414 - 418 Burke Road</p>
+                        <p>Camberwell, Vic 3124</p>
+                    </div>
+                </div>
+                {/* Opening Hours */}
+                <div className={styles.textBlockWrapper}>
+                    <p><strong>Open 7 Days</strong></p>
+                    <div className={styles.lowerText}>
+                        <p>7:30am - 3pm Weekdays</p>
+                        <p>8am - 3pm Weekends</p>
+                    </div>
+                </div>
+                {/* Contact Info */}
+                <div className={styles.textBlockWrapper}>
+                    <p><strong>Get In Touch</strong></p>
+                    <div className={styles.lowerText}>
+                        <p>info@thebakerswife.com.au</p>
+                        <p>0410 850 414</p>
+                    </div>
+                </div>
 
+            </div>
         </div>
     )
 }
