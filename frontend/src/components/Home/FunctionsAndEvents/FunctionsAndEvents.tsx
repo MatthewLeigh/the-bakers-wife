231b8b4113aa1e6827fa3f5dca9a8100dc177f73
MatthewLeigh <matthewcross97@gmail.com>
2025-03-16 11:49:22 +1100

    Draft FunctionsAndEvents component

--- frontend/src/components/Home/FunctionsAndEvents/FunctionsAndEvents.tsx
+++ frontend/src/components/Home/FunctionsAndEvents/FunctionsAndEvents.tsx
@@ -2,9 +2,61 @@
 // Styling
 import styles from "./FunctionsAndEvents.module.css"
 
+// Components
+import Button from "components/Global/Button/Button"
+
+
 const FunctionsAndEvents = () => {
     return (
-        <div>Functions and Events</div>
+        <div className={styles.componentWrapper}>
+
+            {/* Text Wrapper */}
+            <div className={styles.textWrapper}>
+                <h2>Functions & Events</h2>
+                <p>
+                    From bat mitsvas, to weddings, to corporate events, we have an unrivalled event
+                    space for any occasion. From bat mitsvas, to weddings, to corporate events, we
+                    have an unrivalled event space for any occasion.
+                </p>
+            </div>
+
+            {/* Image Wrapper */}
+            <div className={styles.imgWrapper}>
+                <img
+                    className={styles.img}
+                    src="placeholder.jpg"
+                    alt="placeholder"
+                />
+                <img
+                    className={styles.img}
+                    src="placeholder.jpg"
+                    alt="placeholder"
+                />
+                <img
+                    className={styles.img}
+                    src="placeholder.jpg"
+                    alt="placeholder"
+                />
+                <img
+                    className={styles.img}
+                    src="placeholder.jpg"
+                    alt="placeholder"
+                />
+                <img
+                    className={styles.img}
+                    src="placeholder.jpg"
+                    alt="placeholder"
+                />
+            </div>
+
+            {/* Button */}
+            <div className={styles.buttonWrapper}>
+                <Button
+                    text="The Full Package"
+                />
+            </div>
+
+        </div>
     )
 }
 
