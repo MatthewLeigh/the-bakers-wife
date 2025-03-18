6ce891b6f6df7726f13dd42ce099c3927b283197
MatthewLeigh <matthewcross97@gmail.com>
2025-03-18 21:54:49 +1100

    Draft Quote section

--- frontend/src/components/About/Quote/Quote.tsx
+++ frontend/src/components/About/Quote/Quote.tsx
@@ -4,7 +4,17 @@ import styles from "./Quote.module.css"
 
 const Quote = () => {
     return (
-        <div>Quote</div>
+        <div className={styles.componentWrapper}>
+            <blockquote className="hidden">
+                “This is a big quote about something or other. I don’t really know what would be
+                good here, but we can figure that out later”
+            </blockquote>
+            <img
+                className="hidden"
+                src="the-bakers-wife-animation.svg"
+                alt="The Baker's Wife"
+            />
+        </div>
     )
 }
 
