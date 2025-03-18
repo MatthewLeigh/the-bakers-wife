caa40696a7ca407b407371029bf7fc4cfc3f75b6
MatthewLeigh <matthewcross97@gmail.com>
2025-03-18 21:46:15 +1100

    Draft Story component

--- frontend/src/components/About/Story/Story.tsx
+++ frontend/src/components/About/Story/Story.tsx
@@ -1,10 +1,77 @@
 
+// Hooks
+import { useState, useEffect } from "react";
+
 // Styling
 import styles from "./Story.module.css"
 
+// Key Info
 const Story = () => {
+
     return (
-        <div>Story</div>
+        <div className={styles.componentWrapper}>
+
+            {/* Born From Love Wrapper */}
+            <div className={styles.innerWrapper}>
+                <div className={`${styles.textWrapper}`}>
+                    <h2 className="hidden">Born From Love</h2>
+                    <p className="hidden">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea asperiores iusto
+                        culpa molestiae et, minus quisquam pariatur odio earum eius sunt, nam blanditiis
+                        velit dignissimos quo a nihil inventore exercitationem.
+                    </p>
+                    <p className="hidden">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea asperiores iusto
+                        culpa molestiae et, minus quisquam pariatur odio earum eius sunt, nam blanditiis
+                        velit dignissimos quo a nihil inventore exercitationem.
+                    </p>
+                    <p className="hidden">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    </p>
+                    <hr className={`${styles.mtMajor} shortSpacer hidden`}/>
+                </div>
+                <img
+                    className="hidden"
+                    src="images/best-coffee-in-melbourne.jpg"
+                    alt="Best Coffee in Melbourne"
+                />
+                <img
+                    className="hidden"
+                    src="/images/coffee-and-breakfast-bowl.jpg"
+                    alt="placeholder"
+                />
+
+            </div>
+
+            {/* Baked Goods, Baked Good Wrapper */}
+            <div className={styles.innerWrapper}>
+                <img
+                    className="hidden"
+                    src="/images/my-girlfriends-favourite-place-to-eat.jpg"
+                    alt="Delicious Food"
+                />
+                <div className={styles.textWrapper}>
+                    <h2 className="hidden">Baked Goods, Baked Good</h2>
+                    <p className="hidden">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea asperiores iusto
+                        culpa molestiae et, minus quisquam pariatur odio earum eius sunt, nam blanditiis
+                        velit dignissimos quo a nihil inventore exercitationem.
+                    </p>
+                    <p className="hidden">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea asperiores iusto
+                        culpa molestiae et, minus quisquam pariatur odio earum eius sunt, nam blanditiis
+                        velit dignissimos quo a nihil inventore exercitationem.
+                    </p>
+                    <hr className={`${styles.mtMajor} shortSpacer hidden`}/>
+                </div>
+                <img
+                    className="hidden"
+                    src="/images/best-restaurant-in-camberwell.jpg"
+                    alt="Best Restaurant in Camberwell"
+                />
+            </div>
+
+        </div>
     )
 }
 
